22d8e93b8aef3ad8932a337c7627fa0617c6661a
coooIIIII <satviktherobo@gmail.com>
2025-01-27 10:52:34 -0500

    s

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GoLeft.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous (name="GoLeft")
+public class GoLeft extends LinearOpMode{
+    public DcMotor leftFrontDrive = null;  // Done
+    public DcMotor  rightFrontDrive = null; // Done
+    public DcMotor  leftBackDrive = null; // Done
+    public DcMotor  rightBackDrive = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "lfd");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "rfd");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "lbd");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "rbd");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            left(0.5, 5000);
+        }
+
+    }
+    public void left(double power, long time) {
+        leftFrontDrive.setPower(-power);
+        rightFrontDrive.setPower(power);
+        leftBackDrive.setPower(power);
+        rightBackDrive.setPower(-power);
+        sleep(time);
+
+    }
+}
